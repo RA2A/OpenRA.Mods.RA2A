b76eca6e5c55fccc4d82abf3089ca2b2098762ab
Void <yetanotherindieteam@gmail.com>
2017-04-17 16:58:41 +0200

    hopefully changed spaces to tabs

--- Effects/TrackingImage.cs
+++ Effects/TrackingImage.cs
@@ -17,53 +17,55 @@ using OpenRA;
 
 namespace OpenRA.Mods.RA2A.Effects
 {
-    class TrackingImage : IEffect
-    {
-        readonly World world;
-        readonly string palette;
-        readonly Animation anim;
-        readonly bool visibleThroughFog;
-        readonly bool scaleSizeWithZoom;
-        readonly WVec localoffset;
+	class TrackingImage : IEffect
+	{
+		readonly World world;
+		readonly string palette;
+		readonly Animation anim;
+		readonly bool visibleThroughFog;
+		readonly bool scaleSizeWithZoom;
+		readonly WVec localoffset;
 
-        WPos pos;
-        Actor targetactor;
-        public TrackingImage(WPos pos,Actor targetactor, WVec localoffset, World world, string image, string sequence, string palette, bool visibleThroughFog = false, bool scaleSizeWithZoom = false)
-        {
-            this.world = world;
-            this.pos = pos;
-            this.targetactor = targetactor;
-            this.localoffset = localoffset;
-            this.palette = palette;
-            this.scaleSizeWithZoom = scaleSizeWithZoom;
-            this.visibleThroughFog = visibleThroughFog;
-            anim = new Animation(world, image, () => 0);
-            anim.PlayThen(sequence, () => world.AddFrameEndTask(w => w.Remove(this)));            
-        }
+		WPos pos;
+		Actor targetactor;
+		public TrackingImage(WPos pos, Actor targetactor, WVec localoffset, World world, string image, string sequence, string palette, bool visibleThroughFog = false, bool scaleSizeWithZoom = false)
+		{
+			this.world = world;
+			this.pos = pos;
+			this.targetactor = targetactor;
+			this.localoffset = localoffset;
+			this.palette = palette;
+			this.scaleSizeWithZoom = scaleSizeWithZoom;
+			this.visibleThroughFog = visibleThroughFog;
+			anim = new Animation(world, image, () => 0);
+			anim.PlayThen(sequence, () => world.AddFrameEndTask(w => w.Remove(this)));
+		}
 
-        public void Tick(World world)
-        {
-            anim.Tick();
-            WVec offset = localoffset;
-            if (targetactor != null)
-            {
-                var temppos = targetactor.CenterPosition;
-                temppos += localoffset;
-                pos = temppos;
-            } else {
-                var temppos = pos;
-                temppos += offset;
-                pos = temppos;
-            }
-        }
+		public void Tick(World world)
+		{
+			anim.Tick();
+			WVec offset = localoffset;
+			if (targetactor != null)
+			{
+				var temppos = targetactor.CenterPosition;
+				temppos += localoffset;
+				pos = temppos;
+			}
+			else
+			{
+				var temppos = pos;
+				temppos += offset;
+				pos = temppos;
+			}
+		}
 
-        public IEnumerable<IRenderable> Render(WorldRenderer wr)
-        {
-            if (world.FogObscures(pos) && !visibleThroughFog)
-                return SpriteRenderable.None;
+		public IEnumerable<IRenderable> Render(WorldRenderer wr)
+		{
+			if (world.FogObscures(pos) && !visibleThroughFog)
+				return SpriteRenderable.None;
 
-            var zoom = scaleSizeWithZoom ? 1f / wr.Viewport.Zoom : 1f;
-            return anim.Render(pos, WVec.Zero, 0, wr.Palette(palette), zoom);
-        }
-    }
+			var zoom = scaleSizeWithZoom ? 1f / wr.Viewport.Zoom : 1f;
+			return anim.Render(pos, WVec.Zero, 0, wr.Palette(palette), zoom);
+		}
+	}
 }
--- Projectile/TrackingExplosionProjectile.cs
+++ Projectile/TrackingExplosionProjectile.cs
@@ -25,100 +25,101 @@ using OpenRA.Mods.RA2A.Effects;
 
 namespace OpenRA.Mods.RA2A.Projectile
 {
-    // -------------- Disclaimer --------------
-    // This thing is horrible mess and it's going
-    // to be recoded soon. Result of doing this
-    // in hurry.
-    [Desc("Projectile that renders image over the target and explodes after certain duration")]
-    public class TrackingExplosionProjectileInfo : IProjectileInfo
-    {
-        [Desc("Time untill exploding counted after firing weapon")]
-        public readonly int Delay = 100;
+	// -------------- Disclaimer --------------
+	// This thing is horrible mess and it's going
+	// to be recoded soon. Result of doing this
+	// in hurry.
+	[Desc("Projectile that renders image over the target and explodes after certain duration")]
+	public class TrackingExplosionProjectileInfo : IProjectileInfo
+	{
+		[Desc("Time untill exploding counted after firing weapon")]
+		public readonly int Delay = 100;
 
-        [Desc("Image that will be shown above the target untill the delay passes")]
-        public readonly string TrackingImage = "explosion";
+		[Desc("Image that will be shown above the target untill the delay passes")]
+		public readonly string TrackingImage = "explosion";
 
-        [Desc("Image that will be shown above the target untill the delay passes")]
-        public readonly string TrackingImagePalette = "effect";
+		[Desc("Image that will be shown above the target untill the delay passes")]
+		public readonly string TrackingImagePalette = "effect";
 
-        [Desc("List of explosion sequences that can be used.")]
-        [SequenceReference("TrackingImage")]
-        public readonly string[] Explosions = new string[0];
+		[Desc("List of explosion sequences that can be used.")]
+		[SequenceReference("TrackingImage")]
+		public readonly string[] Explosions = new string[0];
 
-        [Desc("TrackingImage offset relative to Actor's center position")]
-        public readonly WVec LocalOffset = WVec.Zero;
+		[Desc("TrackingImage offset relative to Actor's center position")]
+		public readonly WVec LocalOffset = WVec.Zero;
 
-        [Desc("Animation scale in float")]
-        public float Scale = 1f;
+		[Desc("Animation scale in float")]
+		public float Scale = 1f;
 
-        [Desc("Does this have to be scaled dependant on zoom?")]
-        public bool ScaleSizeWithZoom = false;
+		[Desc("Does this have to be scaled dependant on zoom?")]
+		public bool ScaleSizeWithZoom = false;
 
-        public IProjectile Create(ProjectileArgs args) { return new TrackingExplosionProjectile(this, args); }
-    }
-    class TrackingExplosionProjectile : IProjectile
-    {
-        readonly ProjectileArgs args;
+		public IProjectile Create(ProjectileArgs args) { return new TrackingExplosionProjectile(this, args); }
+	}
+	class TrackingExplosionProjectile : IProjectile
+	{
+		readonly ProjectileArgs args;
 
-        string trackingimage;
-        string trackingimagepalette;
-        string[] explosions = new string[0];
-        int delay;
-        WVec localoffset;
-        readonly Animation anim;
-        float scale;
-        bool scaleSizeWithZoom;
-        bool imagealreadyexists = false;
+		string trackingimage;
+		string trackingimagepalette;
+		string[] explosions = new string[0];
+		int delay;
+		WVec localoffset;
+		readonly Animation anim;
+		float scale;
+		bool scaleSizeWithZoom;
+		bool imagealreadyexists = false;
 
-        public TrackingExplosionProjectile(TrackingExplosionProjectileInfo info, ProjectileArgs args)
-        {
-            this.args = args;
-            trackingimage = info.TrackingImage;
-            trackingimagepalette = info.TrackingImagePalette;
-            explosions = info.Explosions;
-            delay = info.Delay;
-            localoffset = info.LocalOffset;
-            scale = info.Scale;
-            scaleSizeWithZoom = info.ScaleSizeWithZoom;
-            imagealreadyexists = false;
-            anim = new Animation(args.SourceActor.World, trackingimage, () => 0);
-        }
+		public TrackingExplosionProjectile(TrackingExplosionProjectileInfo info, ProjectileArgs args)
+		{
+			this.args = args;
+			trackingimage = info.TrackingImage;
+			trackingimagepalette = info.TrackingImagePalette;
+			explosions = info.Explosions;
+			delay = info.Delay;
+			localoffset = info.LocalOffset;
+			scale = info.Scale;
+			scaleSizeWithZoom = info.ScaleSizeWithZoom;
+			imagealreadyexists = false;
+			anim = new Animation(args.SourceActor.World, trackingimage, () => 0);
+		}
 
-        public void Tick(World world)
-        {
-            string explosion = null;
-            // transport.Trait<Cargo>().Passengers.Contains(myPassenger)
-            // args.GuidedTarget.Actor.Trait<Passenger>().Transport != null
-            if (explosions != null){
-                explosion = explosions.RandomOrDefault(Game.CosmeticRandom);// picks random explosion from array 
-            }
-            if (trackingimage != null && explosions != null) // if they exist render
-            {
-                if (imagealreadyexists == false)
-                {
-                    if (args.GuidedTarget.IsValidFor(args.SourceActor) && args.GuidedTarget.Type == TargetType.Actor)
-                        world.AddFrameEndTask(w => w.Add(new TrackingImage(args.GuidedTarget.CenterPosition,args.GuidedTarget.Actor, localoffset, world, trackingimage, explosion, trackingimagepalette, false, scaleSizeWithZoom)));
-                    else
-                        world.AddFrameEndTask(w => w.Add(new TrackingImage(args.PassiveTarget, null, localoffset, world, trackingimage, explosion, trackingimagepalette, false, scaleSizeWithZoom)));
-                    imagealreadyexists = true;
-                }
-            }
-            if (--delay <= 0)
-            {
-                // TODO: implement ra2 transport + bomb logic on bool by freezing delay if in cargo
-                WPos target;
-                if (args.GuidedTarget.IsValidFor(args.SourceActor) && args.GuidedTarget.Type == TargetType.Actor)
-                    target = args.GuidedTarget.CenterPosition + localoffset;
-                else
-                    target = args.PassiveTarget + localoffset;
-                world.AddFrameEndTask(w => w.Remove(this));
-                args.Weapon.Impact(Target.FromPos(target), args.SourceActor, args.DamageModifiers);
-            }
-        }
+		public void Tick(World world)
+		{
+			string explosion = null;
+			// transport.Trait<Cargo>().Passengers.Contains(myPassenger)
+			// args.GuidedTarget.Actor.Trait<Passenger>().Transport != null
+			if (explosions != null)
+			{
+				explosion = explosions.RandomOrDefault(Game.CosmeticRandom);// picks random explosion from array 
+			}
+			if (trackingimage != null && explosions != null) // if they exist render
+			{
+				if (imagealreadyexists == false)
+				{
+					if (args.GuidedTarget.IsValidFor(args.SourceActor) && args.GuidedTarget.Type == TargetType.Actor)
+						world.AddFrameEndTask(w => w.Add(new TrackingImage(args.GuidedTarget.CenterPosition, args.GuidedTarget.Actor, localoffset, world, trackingimage, explosion, trackingimagepalette, false, scaleSizeWithZoom)));
+					else
+						world.AddFrameEndTask(w => w.Add(new TrackingImage(args.PassiveTarget., null, localoffset, world, trackingimage, explosion, trackingimagepalette, false, scaleSizeWithZoom)));
+					imagealreadyexists = true;
+				}
+			}
+			if (--delay <= 0)
+			{
+				// TODO: implement ra2 transport + bomb logic on bool by freezing delay if in cargo
+				WPos target;
+				if (args.GuidedTarget.IsValidFor(args.SourceActor) && args.GuidedTarget.Type == TargetType.Actor)
+					target = args.GuidedTarget.CenterPosition + localoffset;
+				else
+					target = args.PassiveTarget + localoffset;
+				world.AddFrameEndTask(w => w.Remove(this));
+				args.Weapon.Impact(Target.FromPos(target), args.SourceActor, args.DamageModifiers);
+			}
+		}
 
-        public IEnumerable<IRenderable> Render(WorldRenderer wr)
-        {
-            yield break;
-        }
-    }
+		public IEnumerable<IRenderable> Render(WorldRenderer wr)
+		{
+			yield break;
+		}
+	}
 }
